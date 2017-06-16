718d6f2ba65a5e7a8057fd525dbc2d43b62761ee
Sepehr M <voidminded@gmail.com>
2017-06-15 23:42:32 -0700

    Laser data received successfully in Environment

--- src/tools/openAI_gym_env/gym-gazebo/gym_gazebo/envs/kobuki_env.py
+++ src/tools/openAI_gym_env/gym-gazebo/gym_gazebo/envs/kobuki_env.py
@@ -4,11 +4,28 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 #JdeRobot stuff
+import easyiceconfig as EasyIce
+import jderobotComm as comm
+from jderobotTypes import LaserData
 
 #The rest
 import numpy as np
 import random
+import sys
 
 class KobukiEnv( gym.Env):
+  metadata = {'render.modes': ['human']}
+
   def __init__( self):
-    print "initialized"
+    #initializing laser scanner from config file:
+    ic = EasyIce.initialize(["KobukiEnv", "kobuki_conf.cfg"])
+    ic, node = comm.init(ic)
+    robot_laser = comm.getLaserClient(ic, "kobuki.Laser")
+    
+    #initializing the environment:
+    laser = robot_laser.getLaserData()
+    self.observation_dims = [ len(laser.values)]
+    self.observation_space = spaces.Discerete( self.observation_dims[0])
+    print "Inited !"
+
+  
