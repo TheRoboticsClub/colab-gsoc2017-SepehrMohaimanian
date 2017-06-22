4781de02e10bc14f810e50572860b76d703727d6
Sepehr M <voidminded@gmail.com>
2017-06-21 23:18:37 -0700

    Finished initial implementation with laser and 3 actions, yet to be tested

--- src/tools/openAI_gym_env/gym-gazebo/gym_gazebo/envs/kobuki_env.py
+++ src/tools/openAI_gym_env/gym-gazebo/gym_gazebo/envs/kobuki_env.py
@@ -7,6 +7,7 @@ from gym.utils import seeding
 import easyiceconfig as EasyIce
 import jderobotComm as comm
 from jderobotTypes import LaserData
+from jderobotTypes import CMDVel
 
 #The rest
 import numpy as np
@@ -17,15 +18,48 @@ class KobukiEnv( gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__( self):
-    #initializing laser scanner from config file:
     ic = EasyIce.initialize(["KobukiEnv", "kobuki_conf.cfg"])
     ic, node = comm.init(ic)
-    robot_laser = comm.getLaserClient(ic, "kobuki.Laser")
+    #initializing laser scanner from config file:
+    self.laser_client = comm.getLaserClient(ic, "kobuki.Laser")
+    #initializing motors from config file:
+    self.motors_client = comm.getMotorsClient(ic, "kobuki.Motors")
     
     #initializing the environment:
-    laser = robot_laser.getLaserData()
+    laser = self.laser_client.getLaserData()
     self.observation_dims = [ len(laser.values)]
-    self.observation_space = spaces.Discerete( self.observation_dims[0])
+    self.observation_space = spaces.Discerete( self.observation_dims)
+    self.laser = np.zeros(self.observation_dims, np.float32)
+    self.action_space = spaces.Discrete( 3)
+    self.collision = False
+    self.obstale_threshold = 0.0
     print "Inited !"
 
+  def _step(self, action):
+    self.actionToVel( action)
+    reward = 0
+    if self.collision:
+      reward = -99
+    else:
+      if action == 1:
+        reward = 3
+      else
+        reward = 0.6
+    info = {}
+    self.getUpdate()
+    return self.laser, reward, self.collision, info
+
+  def actionToVel( action):
+    action -= 1
+    vel = CMDVel()
+    vel.vx = 0.3
+    vel.az = action*0.3
+    self.motors_client.sendVelocities(vel)
   
+  def getUpdate( self):
+    laser = self.laser_client.getLaserData()
+    self.collision = False
+    for i in range( len(laser.values)):
+      self.laser[i] = float( laser.values[i])/laser.maxRange
+      if self.laser[i] <= self.obstale_threshold:
+        self.collision = True
