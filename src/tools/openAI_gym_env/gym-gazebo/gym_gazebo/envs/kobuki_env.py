f8e58f843dc7fe7e854a636c2212bb1981eaccc5
Voidminded <voidminded@gmail.com>
2017-07-18 11:52:36 -0700

    Finalized env for training

--- src/tools/openAI_gym_env/gym-gazebo/gym_gazebo/envs/kobuki_env.py
+++ src/tools/openAI_gym_env/gym-gazebo/gym_gazebo/envs/kobuki_env.py
@@ -13,6 +13,7 @@ from jderobotTypes import CMDVel
 import numpy as np
 import random
 import sys
+import time
 
 class KobukiEnv( gym.Env):
   metadata = {'render.modes': ['human']}
@@ -25,30 +26,33 @@ class KobukiEnv( gym.Env):
     #initializing motors from config file:
     self.motors_client = comm.getMotorsClient(ic, "kobuki.Motors")
     #initializing gazebo resetter from config file:
-    self.gazebo_resetter =  = comm.getGazeboActionClient(ic, "kobuki.Reset")  
+    self.gazebo_resetter =  comm.getGazeboActionClient(ic, "kobuki.Reset")  
     
     #initializing the environment:
     laser = self.laser_client.getLaserData()
+    print "--------------------------------"
+    print laser
     self.observation_dims = [ len(laser.values)]
-    self.observation_space = spaces.Discerete( self.observation_dims)
+    self.observation_space = spaces.Discrete( self.observation_dims)
     self.laser = np.zeros(self.observation_dims, np.float32)
     self.action_space = spaces.Discrete( 3)
     self.collision = False
-    self.obstale_threshold = 0.0
+    self.obstale_threshold = 1.0
     print "Inited !"
 
   def _step(self, action):
     self.actionToVel( action)
+    time.sleep(0.03)
+    self.getUpdate()
     reward = 0
     if self.collision:
-      reward = -99
+      reward = -9999
     else:
       if action == 1:
-        reward = 3
-      else
-        reward = 0.6
+        reward = 0.09
+      else:
+        reward = 0.003
     info = {}
-    self.getUpdate()
     return self.laser, reward, self.collision, info
 
   def _reset(self):
@@ -56,19 +60,24 @@ class KobukiEnv( gym.Env):
     vel.vx = 0.0
     vel.az = 0.0
     self.motors_client.sendVelocities(vel)
-    self.gazebo_resetter.sendReset() 
+    self.gazebo_resetter.sendReset()
+    self.getUpdate()
+    return self.laser, 0,0 
 
-  def actionToVel( action):
+  def actionToVel( self, action):
     action -= 1
     vel = CMDVel()
-    vel.vx = 0.3
-    vel.az = action*0.3
+    if action == 0:
+      vel.vx = 0.9
+    else:
+      vel.vx = 0.6
+    vel.az = action*0.9
     self.motors_client.sendVelocities(vel)
   
   def getUpdate( self):
     laser = self.laser_client.getLaserData()
     self.collision = False
     for i in range( len(laser.values)):
-      self.laser[i] = float( laser.values[i])/laser.maxRange
-      if self.laser[i] <= self.obstale_threshold:
+      self.laser[i] = laser.values[i] #float( laser.values[i])/laser.maxRange
+      if self.laser[i] < self.obstale_threshold:
         self.collision = True
