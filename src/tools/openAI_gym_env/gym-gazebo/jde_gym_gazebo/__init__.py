d106435354ed118f6c80180835da07ec30fa2904
Sepehr MohaimenianPour <Voidminded@gmail.com>
2017-08-03 21:44:34 -0700

    2D laser env
    * Initial commit
    
    * Laser data received successfully in Environment
    
    * [HOTFIX] Solved bug in laserclient
    
    The bug was giving integers instead of floats as readings of the laser values.
    
    * Finished initial implementation with laser and 3 actions, yet to be tested
    
    * Finished with reset development branch (#1)
    
    * [issue #782] added options to generate only ice packages
    
    * [issue #816] Solved error compilling in ARM
    
    * [issue #815] Solved bug in easyiceconfig_py
    
    * [issue #831] Added new holonomic cars
    
    * [issue #831] Added plugin to control new holonomic cars
    
    * [Fix #836] swapped joysticks: xy position in left and z/turn in right
    
    * Update CMakeLists.txt
    
    * [Issue #835] added vacuum world
    
    * compile c++ run time library
    
    * remove debug message
    
    * [issue 834] Updated evicam_driver to use ptmotors interface
    
    * [Issue #834] Deleted obsolete interfaces
    
    * [issue #834] Deleted wiimote driver of deps
    
    * [issue #834] Delete naoserver and teleoperator
    
    * [issue #833] Added limits to uav_viewer
    
    * we unset application paint property of configfile dialog to have a default background
    
    * [issue #821] Created pantilt_teleop
    
    * [issue #848] Modified configure_module_python macro
    
    * [issue #821] solved Error in ptmotors of parallelIce_py
    
    * [issue #833] Solved problem with max values of cmdvel in uav_viewer
    
    * Fixed debian compilation
    
    * Lights are needed in the 3D_reconstruction world #853
    
    * #857 fixed ns and rgbd support
    
    * #859 Included a simple python cameraviewer for ice
    
    * if roscpp is found install ROS python clients
    
    * #863 included optional horizontal flip for openniserver
    
    * [issue #807] Added MBot model for gazebo simulator
    
    * [issue #807] disable pose3d plugin loading
    
    * [issue #807] disable motors plugin
    
    * [issue #807] Fix robot name
    
    * put exception throwing inside else part of the ifdef otherwise it throws exception on all cases
    
    * Hotfix Added motors to mbot model
    
    * middle of the way
    
    * Reset added and compiling ?
    
    * Reset working fine now
    
    * visualHFSM name is changed to visualStates name in the source code
    
    * change name visualHFSM in variable and class names to visualStates
    
    * [Issue #876] Added samples folder and moved al examples to it
    
    * Am I going to break it ?!
    
    * Finalized env for training
    
    * Changed the way I was waiting for the Gazebo to do an action and make sure state is changed
    
    * Set a random value for each laser messge's timeStamp
    
    * Env update
    
    * Static path for config file removed
    
    * refactored the environment files
    
    * Added the laser2D environment

--- src/tools/openAI_gym_env/gym-gazebo/jde_gym_gazebo/__init__.py
+++ src/tools/openAI_gym_env/gym-gazebo/jde_gym_gazebo/__init__.py
@@ -4,3 +4,8 @@ register(
   id='jde-gazebo-kobuki-laser-v0',
   entry_point='jde_gym_gazebo.envs:KobukiLaserEnv',
 )
+
+register(
+  id='jde-gazebo-kobuki-laser2D-v0',
+  entry_point='jde_gym_gazebo.envs:KobukiLaser2DEnv',
+)
