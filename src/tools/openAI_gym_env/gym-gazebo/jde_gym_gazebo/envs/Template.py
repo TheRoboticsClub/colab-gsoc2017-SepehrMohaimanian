e9e54c8f40a008f0fa5d3d189eb2069aaf27dc3e
Voidminded <voidminded@gmail.com>
2017-08-28 12:02:46 -0700

    Renderring using the Gym render, Template added

--- src/tools/openAI_gym_env/gym-gazebo/jde_gym_gazebo/envs/Template.py
+++ src/tools/openAI_gym_env/gym-gazebo/jde_gym_gazebo/envs/Template.py
@@ -20,67 +20,59 @@ class KobukiRGBEnv( gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__( self):
-    ic = EasyIce.initialize(["KobukiRGBEnv", "/local_home/Dev/GSoC/colab-gsoc2017-SepehrMohaimanian/src/tools/openAI_gym_env/gym-gazebo/jde_gym_gazebo/envs/kobuki_conf.cfg"])
+    # Initialize ICE
+    ic = EasyIce.initialize(["YourRGBEnv", "PATH/TO/YOUR/ICE/CONFIG/FILE"])
     ic, node = comm.init(ic)
     #initializing laser scanner from config file:
-    self.laser_client = comm.getLaserClient(ic, "kobuki.Laser")
+    self.laser_client = comm.getLaserClient(ic, "ROBOT.Laser")
     #initializing motors from config file:
-    self.motors_client = comm.getMotorsClient(ic, "kobuki.Motors")
+    self.motors_client = comm.getMotorsClient(ic, "ROBOT.Motors")
     #initializing gazebo resetter from config file:
-    self.gazebo_resetter =  comm.getGazeboActionClient(ic, "kobuki.Reset")  
+    self.gazebo_resetter =  comm.getGazeboActionClient(ic, "ROBOT.Reset")  
     #initializing camera from config file:
-    self.camera_client =  comm.getCameraClient(ic, "kobuki.Camera")  
+    self.camera_client =  comm.getCameraClient(ic, "ROBOT.Camera")  
     
     #initializing the environment:
-    laser = self.laser_client.getLaserData()
-    print "--------------------------------"
-    print laser
-    self.observation_dims = [ len(laser.values), len(laser.values)]
+    # Create your observation space (84x84 is default DQN agent)
+    self.observation_dims = [ 84, 84]
     self.observation_space = spaces.Box(low=0, high=255, shape=( self.observation_dims[0], self.observation_dims[1], 1))
     self.screen = np.zeros((self.observation_dims[0],self.observation_dims[1],3), np.uint8)
     self.image = self.camera_client.getImage()
-    self.action_space = spaces.Discrete( 3)
+    # Crete your action space (either discrete to be mapped to velocities or continous, based on your agent)
+    self.action_space = spaces.Discrete( NUmberOfActions)
+    # Simple viwer for renderring the agent's input
     self.viewer = None
+    # For collision detection in each step
     self.collision = False
+    # Min safe distance from obstacle, if robot gets closer to an obstacle than this distance scenario will be terminated
     self.obstale_threshold = 0.5
     self.timeStamp = 0.0
     self.frames_skip = 0   
-    self.display= True
-    self.pixel_thresh = 60
-    self.l = 0
-    self.r = 0
     print "Inited !"
 
+  # Each step your agent will execute this function for its observation and gets a new obsevation/reward
   def _step(self, action):
+    #For converting the discrete action for robot velocity
     self.actionToVel( action)
+    # Get the sensors update
     self.getUpdate()
+    
+    # here define your reward function
     reward = 0
     if self.collision:
-      reward = -99
+      reward = -99 # A huge penalty for hitting an obstacle
     else:
       if action == 1:
-        reward = 0.6+(self.r*0.9 + self.l*0.9) 
+        reward = 0.6
       else:
-        extra = 0
-        if self.r == 2 and self.l == 2:
-          extra = 1
-        reward = -0.03+extra
-       # if self.r == -1 and self.l == -1:
-       #   reward = 0.03
-       # elif self.r == -1:
-       #   if action == 0:
-       #     reward = 0.9
-       #   else:
-       #     reward = -0.03
-       # else:
-       #   if action == 0:
-       #     reward = -0.03
-       #   else:
-       #     reward = 0.9
+        reward = -0.03
 
+    # Extra information to be sent to agent for debug purposes
     info = {}
+    # Send new observation, this step's reward, if we have collided with an obstacle or not, and extra info to the agent based on its last decision
     return self.screen, reward, self.collision, info
 
+  # Used for resetting the Gazebo when an episode finishes
   def _reset(self):
     vel = CMDVel()
     vel.vx = 0.0
@@ -90,6 +82,7 @@ class KobukiRGBEnv( gym.Env):
     self.getUpdate()
     return self.screen, 0,0 
 
+  # For renderring the observation
   def _render(self, mode='human', close=False):
     if close:
       if self.viewer is not None:
@@ -103,6 +96,7 @@ class KobukiRGBEnv( gym.Env):
         self.viewer = rendering.SimpleImageViewer()
       self.viewer.imshow(img)
 
+  #Converting the action to robot velocities
   def actionToVel( self, action):
     action -= 1
     vel = CMDVel()
@@ -113,27 +107,21 @@ class KobukiRGBEnv( gym.Env):
     vel.az = action*1.2
     self.motors_client.sendVelocities(vel)
   
+  #Read sensory data and convert the to observation 
   def getUpdate( self):
     image = self.camera_client.getImage()
-    #while self.timeStamp == self.camera_client.getImage().timeStamp:
-    #  pass
+    #  in Gazebo/JdeRobot sometimes the camera image data does not update and frame freezes, to avoid srcwing up the training:
     while np.array_equal(image.data ,self.image.data):
       image = self.camera_client.getImage() 
     self.image = image
+    
+    # If you want to skip some frames
     #for i in range(self.frames_skip):
     #  while image.timeStamp == self.camera_client.getImage().timeStamp:
     #    pass
     #  image = self.camera_client.getImage()
-    r_raw = sum(image.data[-1,320:,:] > self.pixel_thresh) 
-    if max(r_raw) == 0:
-      self.r = -1
-    else:
-      self.r = 3-sum( r_raw == max(r_raw))
-    l_raw = sum(image.data[-1,:320,:] > self.pixel_thresh) 
-    if max(l_raw) == 0:
-      self.l = -1
-    else:
-      self.l = 3-sum( l_raw == max(l_raw))
+    
+    # Using laser for collision detection
     laser = self.laser_client.getLaserData()
     self.collision = False
     if np.min( laser.values) < self.obstale_threshold:
